b27980d3e3577ff44a1546bb009d93d8521588e4
Saw-mon-and-Natalie <samangharib50233@gmail.com>
2022-06-16 19:39:55 -0700

    updated workflow

---
+++ .github/scripts/create-or-update-ref.js
@@ -0,0 +1,32 @@
+module.exports = async ({ github, context }, tagName) => {
+  try {
+    await github.rest.getRef({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: `tags/${tagName}`,
+    });
+
+    await github.rest.git.updateRef({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: `tags/${tagName}`,
+      sha: context.sha,
+      force: true,
+    });
+  } catch (err) {
+    console.log(`⛔ tag: ${tagName} doesn't exist. Creating the tag`);
+
+    try {
+      await github.rest.git.createRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: `refs/tags/${tagName}`,
+        sha: context.sha,
+        force: true,
+      });
+    } catch (err) {
+      console.error(`Failed to create tag: ${tagName}`);
+      console.error(err);
+    }
+  }
+};
